19b98bacfecfb27703979f6db637acb533abc566
yuya-maruta <ymx08110112@gmail.com>
2020-08-17 15:34:17 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
+ # before_action :basic_auth
+  before_action :authenticate_user!
   # protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:lastname,:firstname,:lastname_kana,:firstname_kana,:nickname,:birthday,:email,:encrypted_password])
+  end
+
 
   private
 
