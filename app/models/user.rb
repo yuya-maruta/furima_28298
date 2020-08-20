7dd4e0c85d72e6f0f5bf40acf4ac13e2edcd5f96
yuya-maruta <ymx08110112@gmail.com>
2020-08-20 09:05:29 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
 
   #validates :name, presence: true
   with_options presence: true do
-    validates :firstname
-    validates :lastname
-    validates :firstname_kana
-    validates :lastname_kana
+    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]*\z/, message: "は全角で入力してください。"}
+    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]*\z/, message: "は全角で入力してください。"}
+    validates :firstname_kana,format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :lastname_kana,format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
     validates :nickname
     validates :birthday
-    validates :password
+    validates :password,presence: true, length: { minimum: 6}
     validates :email
 
 
@@ -21,4 +21,4 @@ class User < ApplicationRecord
 #has_many :purchases 
 
 end
-#format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+#password.match(/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i)
