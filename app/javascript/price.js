63d71ba7e855c9c2390344b17ee59deb253706f2
yuya-maruta <ymx08110112@gmail.com>
2020-08-23 16:05:47 +0900

    販売手数料・販売利益の計算式設定

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,8 +1,30 @@
-const price = document.getElementById("item-price");
-console.log(price)
+window.addEventListener('load', function(){
 
-const tax = document.getElementById("add-tax-price");
-console.log(tax)
+  const price = document.getElementById("item-price");
+  console.log(price)
 
-const profit = document.getElementById("profit");
-console.log(profit)
+  price.addEventListener('input', function(){
+    console.log("input OK")
+  })
+    
+
+    const tax = document.getElementById("add-tax-price");
+    console.log(tax)
+    
+    function calc(price) {
+      let calc = price * 0.1;
+      return calc
+    }
+    console.log(calc(price))     
+      
+
+    const profit = document.getElementById("profit");
+    console.log(profit)
+  
+    function sale(price) {
+      let sale = price - calc;
+      return sale
+    }
+  console.log(sale(price))   
+  
+})
