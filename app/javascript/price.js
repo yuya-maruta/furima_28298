6a0a672accf3a009f43d286d8278002fa9285fbf
yuya-maruta <ymx08110112@gmail.com>
2020-08-23 12:56:21 +0900

    jsファイルを読み込む設定

---
+++ app/javascript/price.js
@@ -0,0 +1,8 @@
+const price = document.getElementById("item-price");
+console.log(price)
+
+const tax = document.getElementById("add-tax-price");
+console.log(tax)
+
+const profit = document.getElementById("profit");
+console.log(profit)
