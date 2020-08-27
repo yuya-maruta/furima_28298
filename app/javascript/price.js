0421a3ff54b40af90123c1aa9d889a3557a8daa1
yuya-maruta <ymx08110112@gmail.com>
2020-08-27 20:28:02 +0900

    Update price.js

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,10 +3,8 @@ window.addEventListener('load', function(){
   const price = document.getElementById("item-price");
   const tax = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
-
-  console.log(price)
-
-  price.addEventListener("input", (e) => {
+ 
+ price.addEventListener("input", (e) => {
 
     const price_num = document.getElementById("item-price").value;
     console.log(price_num)
@@ -19,34 +17,5 @@ window.addEventListener('load', function(){
     console.log(profit)
     profit.innerHTML = profit_num
   });
-
-
-  // console.log(price)
-  
-  
- 
-  //price.addEventListener('input', function(){
-    //console.log("input OK")
-  //})
-    
-
-  //   const tax = document.getElementById("add-tax-price");
-  //   console.log(tax)
-    
-  //   function calc(price) {
-  //     let calc = price * 0.1;
-  //     return calc
-  //   }
-  //   console.log(calc(price))     
-      
-
-  //   const profit = document.getElementById("profit");
-  //   console.log(profit)
-  
-  //   function sale(price) {
-  //     let sale = price - calc;
-  //     return sale
-  //   }
-  // console.log(sale(price))   
-  
+     
 })
