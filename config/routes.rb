96cb291e8a26ac8d792118e193d87ea041d4b280
yuya-maruta <ymx08110112@gmail.com>
2020-08-27 15:55:07 +0900

    出品者にしか商品の編集・削除のリンクが踏めない記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'items#index'
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update,:show]
   resources :items, only:[:new,:index,:show,:edit,:create]
 
 end
