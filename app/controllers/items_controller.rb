bfbc610eabc65fd9d22a6b3c75faaa1e9a296f80
yuya-maruta <ymx08110112@gmail.com>
2020-08-28 09:51:09 +0900

    編集ボタンからの遷移先指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,22 @@
 class ItemsController < ApplicationController
-  #before_action :set_item, only: [:edit, :show]
 
+  
   def new
     @item = Item.new
   end
   
-  def index
-   
-   @items = Item.all.order("created_at DESC")
   
-
-   
+  def index
+    @items = Item.all.order("created_at DESC")
   end
 
+  
   def create
     @item = Item.new(item_params)
    if @item.valid?
     @item.save
     redirect_to root_path
    else
-    
-
     render :index
    end
 
@@ -31,16 +27,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+
   end
 
 
-  #def set_item
-   # @item = Item.find(params[:id])
-  #end
-
   private
   def item_params
-    #params.require(:item).permit(:item).merge(user_id: current_user.id)
     params.require(:item).permit(:image,:money,:name,:explanation,:category_id,:status_id,:deliveryfee_id,:shoppingregion_id,:deliverytime_id).merge(user_id: current_user.id)
   end
 
