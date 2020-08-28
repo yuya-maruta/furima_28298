fc09ded3b6da2614230c2b6db0bf45aad8ad57dc
yuya-maruta <ymx08110112@gmail.com>
2020-08-28 19:06:28 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update,:destroy]
 
   
   def new
@@ -37,9 +37,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.user_id == current_user.id
-       item.destroy 
+    if @item.user_id == current_user.id
+       @item.destroy 
        redirect_to root_path
      end
   end
