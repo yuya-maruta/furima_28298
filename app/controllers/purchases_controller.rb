cbc1cb7ecf40340f9f254f9687d3c40f12fc5e8c
yuya-maruta <ymx08110112@gmail.com>
2020-09-01 21:35:53 +0900

    インデントの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,44 +8,43 @@ class PurchasesController < ApplicationController
   end
 
 
-def create
+  def create
   
-  @user_purchases = UserPurchases.new(purchases_params)
-   if @user_purchases.valid?
-    pay_item
-    @user_purchases.save
-    return redirect_to root_path
-  else
-    @item = Item.find(params[:item_id])
-    render 'index'
-  end
+    @user_purchases = UserPurchases.new(purchases_params)
+      if @user_purchases.valid?
+        pay_item
+        @user_purchases.save
+        return redirect_to root_path
+      else
+        @item = Item.find(params[:item_id])
+        render 'index'
+    end
  
-end
+  end
 
 
 private
 
- def purchases_params
-  params.permit(:name, :postalcode, :prefecture, :city, :house_number, :building_name, :phone_number,:token, :item_id).merge(user_id: current_user.id )
- end
-
- 
-
-def move_to_index
-  unless user_signed_in?
-    redirect_to "/"
+  def purchases_params
+    params.permit(:name, :postalcode, :prefecture, :city, :house_number, :building_name, :phone_number,:token, :item_id).merge(user_id: current_user.id )
   end
-end
+
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to "/"
+    end
+  end
 
 
-def pay_item
-  @item = Item.find(params[:item_id])
-  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  Payjp::Charge.create(
-    amount: @item.money, 
-    card: purchases_params[:token],    
-    currency:'jpy'                
-  )
-end
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.money, 
+      card: purchases_params[:token],    
+      currency:'jpy'                
+    )
+  end
 
 end
