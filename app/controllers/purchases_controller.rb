68747f1966c90b51efa33f475357251fd64bf1d6
yuya-maruta <ymx08110112@gmail.com>
2020-09-03 12:15:01 +0900

    購入機能の修正②

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,6 +5,11 @@ class PurchasesController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
+    
+    if @item.user_id == current_user.id || @item.purchase != nil  
+      redirect_to root_path
+    end
+    
   end
 
 
